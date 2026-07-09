42d6236922a44107c3c2ad3f54c4c13fcacb9e00
jonatasperaza <jonatassilvaperaza@gmail.com>
2026-07-09 19:39:20 -0300

    Fix: atualizando idade nos SVGs

--- today.py
+++ today.py
@@ -322,6 +322,7 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     """
     tree = etree.parse(filename)
     root = tree.getroot()
+    justify_format(root, 'age_data', age_data, 49)
     justify_format(root, 'commit_data', commit_data, 22)
     justify_format(root, 'star_data', star_data, 14)
     justify_format(root, 'repo_data', repo_data, 6)
