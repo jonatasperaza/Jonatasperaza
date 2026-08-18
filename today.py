8aa9643040e02bbe21dac15382feb138b4c72568
jonatasperaza <jonatassilvaperaza@gmail.com>
2026-08-18 14:27:35 -0300

    Fix: update example user data in comments for clarity

--- today.py
+++ today.py
@@ -444,7 +444,7 @@ if __name__ == '__main__':
     """
     print('Calculation times:')
     # define global variable for owner ID and calculate user's creation date
-    # e.g {'id': 'MDQ6VXNlcjU3MzMxMTM0'} and 2019-11-03T21:15:07Z for username 'Jonatasperaza'
+    # e.g {'id': 'MDQ6VXNlcjczOTcwNTE5'} and 2020-11-04T23:08:44Z for username 'jonatasperaza'
     user_data, user_time = perf_counter(user_getter, USER_NAME)
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
@@ -459,7 +459,7 @@ if __name__ == '__main__':
     follower_data, follower_time = perf_counter(follower_getter, USER_NAME)
 
     # several repositories that I've contributed to have since been deleted.
-    if OWNER_ID == {'id': 'MDQ6VXNlcjU3MzMxMTM0'}: # only calculate for user Andrew6rant
+    if OWNER_ID == {'id': 'MDQ6VXNlcjU3MzMxMTM0'}: # upstream owner (Andrew6rant) only - the archived data below belongs to that account
         archived_data = add_archive()
         for index in range(len(total_loc)-1):
             total_loc[index] += archived_data[index]
